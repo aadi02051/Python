dfba04a5ad387a044d7bc0d3b7925a271f94695d
aadi02051 <adityatyagi230@gmail.com>
2022-12-25 19:12:21 +0000

    test1

--- main.py
+++ main.py
@@ -17,3 +17,7 @@ class Phone(Details):
 Phone1 = Phone('Jio',500,5)
 Phone1.broken_phone = 2
 print(Phone1.Number_of_phones_not_damaged(Phone1.total_phones,Phone1.broken_phone))
+
+Phone2 = Phone('Samsung',1500,15)
+Phone2.broken_phone = 3
+print(Phone1.Number_of_phones_not_damaged(Phone2.total_phones,Phone2.broken_phone))
